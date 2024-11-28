d85fa6f8594cd1b1ea229100370ea82049f742ec
shwetayadav16 <124680771+shwetayadav16@users.noreply.github.com>
2024-11-28 19:02:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of Two Sorted Arrays with Duplicate Elements/union-of-two-sorted-arrays-with-duplicate-elements.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        // return vector with correct order of elements
+        vector<int>result;
+        set<int>st;
+        for(int num:a){
+            st.insert(num);
+        }
+        for(int num:b){
+            st.insert(num);
+        }
+        for(auto num:st){
+            result.push_back(num);
+        }
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a, b;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.findUnion(a, b);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
